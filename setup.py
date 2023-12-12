e9444678f775cedb8b509387cb1c8515ee5ef6e1
shashiKumar14 <egashashikumar@gmail.com>
2023-12-13 00:02:34 +0530

    new code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simplemlapp-with-ci-dvc",
-    version="0.0.1",
+    version="0.0.2",
     description="its a wine Q package", 
     author="shashiKumar14", 
     packages=find_packages(),
